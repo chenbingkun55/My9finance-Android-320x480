16a49416a3d1cfd721e7cba417d0b6875f2c7668
chenbk <chenbingkun55@163.com>
2013-01-18 10:36:03 +0800

    添加多个Index.html页面.

--- skin/android_320x480/foot.php
+++ skin/android_320x480/foot.php
@@ -2,7 +2,7 @@
 <div class="Foot FootFont" align="right">
 	<INPUT class="LogoutButton" type="submit" onclick="logout();" value="<?PHP echo $_LOGOUT?>"><BR>
     <CENTER>
-		Copyright © 2010-2012 ChenBK All Rights Reserved<br> 
+		Copyright © 2010-2013 ChenBK All Rights Reserved<br> 
 		一个简单在线个人收支管理系统<br>
 		E-mail : <a href="mailto:chenbingkun55@163.com">ChenBingKun55@163.com</a> 
 	</CENTER>
